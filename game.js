82b287629206beacfb69a89a6bcd5f9c3b66733c
nebula084 <95935025+nebula084@users.noreply.github.com>
2023-05-14 18:38:19 -0700

    2 files modified

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.WEBGL,
+  backgroundColor: 0xffc836,
+  width: 440,
+  height: 550,
+  scene: {
+    create,
+  },
 };
 
 const game = new Phaser.Game(config);
